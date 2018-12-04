c3a0d1b8bdd30391126ccd4ec795af4773743a43
Victor Martin <leon.truc@gmail.com>
2018-12-04 12:02:54 +0100

    Debut class domino

--- bluej/Domino.java
+++ bluej/Domino.java
@@ -5,6 +5,7 @@
  * @author (votre nom)
  * @version (un numéro de version ou une date)
  */
+import java.util.*;
 public class Domino
 {
     /*
@@ -18,21 +19,15 @@ public class Domino
      * 7=Mine
      */
     
-    public domino()
+    private HashMap<Integer,ArrayList<Integer>> Domino = new HashMap<Integer,ArrayList<Integer>>(); 
+    
+    public Domino()
     {
         // initialisation des variables d'instance
-        x = 0;
+        
     }
 
-    /**
-     * Un exemple de méthode - remplacez ce commentaire par le vôtre
-     *
-     * @param  y   le paramètre de la méthode
-     * @return     la somme de x et de y
-     */
-    public int sampleMethod(int y)
-    {
-        // Insérez votre code ici
-        return x + y;
+    public ArrayList<Integer> returnDomino(int num){
+        return Domino.get(num);
     }
 }
