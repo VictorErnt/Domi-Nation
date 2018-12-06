c7dada958f4f391a51e833ba52a39ac71195372e
Victor Martin <leon.truc@gmail.com>
2018-12-06 10:27:17 +0100

    Class Domino

--- bluej/Domino.java
+++ bluej/Domino.java
@@ -10,6 +10,7 @@ public class Domino
 {
     /*
      * Type:
+     * 0=Rien
      * 1=Chateau
      * 2=Mer
      * 3=Foret
@@ -18,16 +19,63 @@ public class Domino
      * 6=Champs
      * 7=Mine
      */
+    Random random= new Random();
+    private HashMap<Integer,ArrayList<Integer>> biblioDomino = new HashMap<Integer,ArrayList<Integer>>(); 
     
-    private HashMap<Integer,ArrayList<Integer>> Domino = new HashMap<Integer,ArrayList<Integer>>(); 
+    //Faire le traitement du .csv pour le transformer en bilbliotéque
+    
+    private final HashMap<Integer,ArrayList<Integer>> refDomino =biblioDomino;//Pour aller chercher la valur d'un domino
+    Stack rdnpioche = new Stack();
     
     public Domino()
     {
-        // initialisation des variables d'instance
+        
+        
+        //Donner le nombre de domino à utiliser
         
     }
 
     public ArrayList<Integer> returnDomino(int num){
-        return Domino.get(num);
+        return refDomino.get(num);
     }
+    
+    public ArrayList<Integer> rangerDomino(ArrayList<Integer> list0){
+        Collections.sort(list0);
+        return list0;
+    }
+    
+    public void iniPioche(int nbDomidujeu){
+        int r=0;
+        int compteur=0;
+
+        int domiPioche=0;
+        int L=0; 
+        int longueur = biblioDomino.size();
+        for(int i=0; i<nbDomidujeu;i++) {
+            Collection<Integer> key=biblioDomino.keySet();
+            L=key.size();
+            do {
+                r=random.nextInt(L);
+            }while(r==0);
+            Iterator<Integer> it=key.iterator();
+            compteur=0;
+            while(compteur<r) {
+                compteur++;
+                domiPioche=it.next();
+            }
+            this.rdnpioche.push(domiPioche);
+            biblioDomino.remove(domiPioche);
+        }
+        
+    }
+    
+    public int Pioche(){
+        int a=(int)rdnpioche.peek();
+        rdnpioche.pop();
+        return a;
+    }
+   
 }
+    
+    
+
