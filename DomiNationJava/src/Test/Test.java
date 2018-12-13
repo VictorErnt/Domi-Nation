83d0a5e73ecbcfd7a408b7bdabc7d92f5dfe0d63
Victor Martin <leon.truc@gmail.com>
2018-12-13 09:09:06 +0100

    code java

---
+++ DomiNationJava/src/Test/Test.java
@@ -0,0 +1,19 @@
+package Test;
+import java.util.Stack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Stack<Integer> rdnpioche=new Stack<Integer>();
+		rdnpioche.push(25);
+		rdnpioche.push(25);
+		rdnpioche.push(25);
+		rdnpioche.push(25);
+		rdnpioche.push(25);
+		
+		rdnpioche.size();
+	}
+
+}
